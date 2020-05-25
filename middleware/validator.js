f4de1c5ac5b674837e061ef4b0b36c349fc9554b
profprogrammeur <contact@profprogrammeur.com>
2020-05-25 11:05:50 -0300

    clean models

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,26 +1,4 @@
-// const { check, validationResult } = require('express-validator');
 
-// module.exports = (req, res, next) => {
-//     try {
-//         console.log(req.body.email)
-//     // [
-//     //     // username must be an email
-//     //     check(req.body.email).isEmail(),
-//     //     // password must be at least 5 chars long
-//     //     check(req.body.password).isLength({ min: 5 })
-//     // ], (req, res) => {
-//     //     // Finds the validation errors in this request and wraps them in an object with handy functions
-//     //     const errors = validationResult(req);
-//     //     if (!errors.isEmpty()) {
-//     //         return res.status(422).json({ errors: errors.array() });
-//     //     }} 
-
-//     } catch {
-//         res.status(401).json({
-//             error: new Error('Invalid request!')
-//         });
-//     }
-// };
 
 const { body, validationResult } = require('express-validator')
 const userValidationRules = () => {
