df32f55ef4863ad6f0d72133120c8ae2f19abf07
profprogrammeur <contact@profprogrammeur.com>
2020-06-01 12:09:04 -0300

    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ mongoose.connect('mongodb+srv://antra:'+mongoKey+'@antcluster-zvn53.gcp.mongodb.
     //.use process all kind of request     
     //header definitions allow CORS  (Cross Origin Ressource Sharing)
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
